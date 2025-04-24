4554c595a76f4957de2c0142ef4908552e2a5ed5
Anand1997 <anandnarawade.an@gmail.com>
2025-04-25 00:48:13 +0530

    [ REFCODE ] Trying to parse commands.

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,58 @@
 #include <iostream>
 #include "../lib/cxxopts.hpp"
 
-int main(int argc, char** argv)
-{
-    cxxopts::Options options("MyProgram", "Description of MyProgram");
-    options.add_options()
-        ("d,debug", "Enable debugging")  // Boolean flag
-        ("i,integer", "Integer parameter", cxxopts::value<int>())
-        ("f,file", "File name", cxxopts::value<std::string>())
-        ("v,verbose", "Verbose output", cxxopts::value<bool>()->default_value("false"));
+int main(int argc, char* argv[]) {
+    cxxopts::Options options("mygit", "A simple Git-like CLI tool");
     
+    // both can not work together
+    /*options.add_options()
+        ("h,help", "Show help")
+        ("v,version", "Show version");
+
     auto result = options.parse(argc, argv);
-    
-    if (result.count("debug")) {
-        std::cout << "Debugging enabled!" << std::endl;
+    if (result.count("help")) {
+        std::cout << options.help() << std::endl;
+        return 0;
     }
-    if (result.count("integer")) {
-        std::cout << "Integer parameter: " << result["integer"].as<int>() << std::endl;   
-        int integerValue = result["integer"].as<int>();
+    if (result.count("version")) {
+        std::cout << "mygit version 1.0" << std::endl;
+        return 0;
+    }*/
+
+    if (argc > 1) {
+       std::string command = argv[1];
+
+       if (command == "commit") {
+           cxxopts::Options commitOptions("commit", "Commit changes");
+           commitOptions.add_options()
+               ("m,message", "Commit message", cxxopts::value<std::string>())
+               ("a,all", "Commit all changes");
+
+           auto commitResult = commitOptions.parse(argc - 1, argv + 1);
+
+           if (commitResult.count("message")) {
+               std::cout << "Committing with message: " << commitResult["message"].as<std::string>() << std::endl;
+           }
+
+           if (commitResult.count("all")) {
+               std::cout << "Committing all changes!" << std::endl;
+           }
+       } else if (command == "push") {
+           cxxopts::Options pushOptions("push", "Push changes");
+           pushOptions.add_options()
+               ("r,remote", "Remote repository", cxxopts::value<std::string>())
+               ("b,branch", "Branch name", cxxopts::value<std::string>());
+
+           auto pushResult = pushOptions.parse(argc - 1, argv + 1);
+
+           std::cout << "Pushing to remote: " << pushResult["remote"].as<std::string>()
+                     << " on branch: " << pushResult["branch"].as<std::string>() << std::endl;
+       } else {
+           std::cout << "Unknown command: " << command << std::endl;
+       }
+    } else {
+       std::cout << "No command provided. Use --help for usage." << std::endl;
     }
-    if (result.count("verbose")) {
-        bool verbose = result["verbose"].as<bool>();
-        std::cout << "Verbose mode: " << (verbose ? "enabled" : "disabled") << std::endl;
-    }
-    if (result.count("file")) {
-        std::string fileName = result["file"].as<std::string>();
-        std::cout << "File name: " << fileName << std::endl;
-    }
+
     return 0;
 }
